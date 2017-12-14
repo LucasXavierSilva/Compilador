ee7315f645528ef096c46e22e6e99e0f8055a007
LucasXavierSilva <luc4s02@gmail.com>
2017-12-14 17:56:51 -0200

    Symbol Table Search
    Added a function that looks if the symbol that is being added is already in the Symbol Table.

--- ProjetoCompilador/compilador.c
+++ ProjetoCompilador/compilador.c
@@ -14,6 +14,7 @@ typedef struct list {
   char *name;
   char *type;
   char *value;
+  char *sizeLimit;
   struct list *next;
 } symbolTable;
 symbolTable *root = NULL, *leaf = NULL; 
@@ -21,21 +22,23 @@ symbolTable *root = NULL, *leaf = NULL;
 char *removeSpaces (char *input);
 char *splitChar(char *line, char *firstDelimiter, char *secondDelimiter);
 char *splitLine (char *input);
-symbolTable *insertSymbol(char *name, char *type, char *value);
-void createList(char *name, char *type, char *value);
-int *checkList(char *name);
+symbolTable *insertSymbol(char *name, char *type, char *value, char *sizeLimit);
+void createList(char *name, char *type, char *value, char *sizeLimit);
 void printList();
+int checkInsertVarName(char *name);
 char checkVariable(char *line);
 bool checkVariableList(char *name);
 bool dataSearch(symbolTable, char *name);
 int checkChar(char *string, char *value);
-int testSemi(const char *str);
+int *checkList(char *name);
+int checkNumber(const char *string);
+int testReserved(char *str);
+int testSemi(const char *str); 
 
 int size = 0;
 
 int main() {
 	setlocale(LC_ALL, "PORTUGUESE");
-	createList("NULL", "NULL", "NULL");
 	int i;
 	int lineNo = 0;
 	FILE* fh;
@@ -77,13 +80,14 @@ int main() {
 	}
 	*/	
 	
-		
+	char name = "teste";
     printList();
 	printf("\nQuantidade de bytes usado: %i",size);
 	free(line);
 	free(root);
 }
 
+//Remove spaces and useless data
 char* removeSpaces (char* input)
 {
     int i,j, aux = 1;
@@ -107,6 +111,7 @@ char* removeSpaces (char* input)
     return output;
 }
 
+//Split the characters by the given delimiter
 char *splitChar(char *line, char *firstDelimiter, char *secondDelimiter)
 {
     const char *s = line;
@@ -145,15 +150,18 @@ char *splitChar(char *line, char *firstDelimiter, char *secondDelimiter)
     //if ( target ) printf( "\n\n\n %s\n\n\n", target );
 }
 
+//Splits the contents in the line
 char *splitLine (char *input)
 {
-	char *token, *string, *name="NULL", *type="NULL", *value="NULL", splitVariable[strlen(input)], splitValue[strlen(input)];
+	char *token, *string, *name="NULL", *type="NULL", *value="NULL", *sizeLimit="NULL", splitVariable[strlen(input)], splitValue[strlen(input)];
 	strcpy(splitVariable, input);
 	strcpy(splitValue, input);
 	int aux = checkChar(input,"#");
-
-	if(strstr(input,"#"))
+	
+	if(strstr(input,"inteiro") || strstr(input,"caractere") || strstr(input,"real"))
 	{
+		if(strstr(input,"#"))
+		{
 		
 		if(strstr(input,"inteiro"))
 		{
@@ -172,67 +180,94 @@ char *splitLine (char *input)
 			printf("Atribuição de variável deve receber tipo de valor inteiro, caractere ou real!");
 			system("pause");
 		}
+		
+		if(testSemi(input))
+		{
+			printf("\nA linha deve finalizar com \";\"");
+			return 0;
+		}
+		
+		while(aux != 0)
+		{
+			value = "NULL";
+			name = "NULL";
+			sizeLimit = "NULL";
+			if(strstr(input,"#"))
+			{	
+				if(!strstr(splitChar(input,"#",","),"ERRO"))
+			    	name = splitChar(input,"#",",");
+			    else if(!strstr(splitChar(input,"#",";"),"ERRO"))
+			    	name = splitChar(input,"#",";");
+			}
+			if(strstr(input,"="))
+			{
+				if(!strstr(splitChar(input,"#","="),"ERRO"))
+				{
+					name = splitChar(input,"#", "=");	
+				}
+					
+				if(!strstr(splitChar(input,"=",","),"ERRO"))
+			  		value = splitChar(input,"=",",");
+			  
+				if(!strstr(splitChar(input,"=",";"),"ERRO"))
+			  		value = splitChar(input,"=",";");
+			  	
+				if(type != "caractere" && strstr(input,"\""))
+				{
+					printf("\nTipo %s não pode receber valor do tipo charactere!", type);
+					return 0;
+				}
+				else if(type == "caractere" && strstr(input,"\""))
+			  		value = splitChar(splitValue,"\"","\"");
+			}		
+			
+			if(strstr(input,"["))
+			{
+				sizeLimit = splitChar(input,"[","]");
+				if(!strstr(splitChar(input,"[","]"),"ERRO") && checkNumber(sizeLimit) == 1)
+				{
+					name = splitChar(input,"#","[");
+				}
+				else if(checkNumber(sizeLimit) == 0)
+				{
+					printf("\nO campo aceita apenas valores numéricos entre \"[\" e \"]\".");
+					return 0;
+				}
+			}
+			if(aux > 1)
+			{
+				input = splitChar(input,",",";");
+				strcat(input, ";");
+			}
+			if(value != "NULL" && strlen(value) > atoi(sizeLimit) && sizeLimit != "NULL")
+			{
+				printf("\nO valor %s é maior do que o limite implicado de [%s]", value, sizeLimit);
+			}
+			/*
+			if(checkList(name) == 1)
+			{
+				printf("Nome já existe na tabela de símbolos: %s", nome);
+				return 0;
+			}*/
+			
+			testReserved(name);
+    		checkInsertVarName(name);
+			insertSymbol(name, type, value, sizeLimit);
+			aux--;
+		}
+
 	}	
+	
+	}
 	else
 	{
-		printf("\nNão é uma declaração de variável!\n");
+		//printf("\nNão é uma declaração de variável!\n");
 		return 0;
-	}
-	if(testSemi(input))
-	{
-		printf("\nA linha deve finalizar com \";\"");
-		return 0;
-	}
-	while(aux != 0)
-	{
-		value = "NULL";
-		name = "NULL";
-		if(strstr(input,"#"))
-		{	
-			if(!strstr(splitChar(input,"#",","),"ERRO"))
-		    	name = splitChar(input,"#",",");
-		    else if(!strstr(splitChar(input,"#",";"),"ERRO"))
-		    	name = splitChar(input,"#",";");
-		    
-		}
-	
-		if(strstr(input,"="))
-		{
-			if(!strstr(splitChar(input,"#","="),"ERRO"))
-				name = splitChar(input,"#", "=");
-				
-			if(!strstr(splitChar(input,"=",","),"ERRO"))
-		  		value = splitChar(input,"=",",");
-		  
-			if(!strstr(splitChar(input,"=",";"),"ERRO"))
-		  		value = splitChar(input,"=",";");
-		  	
-			if(type != "caractere" && strstr(input,"\""))
-			{
-				printf("\nTipo %s não pode receber valor do tipo charactere!", type);
-				return 0;
-			}
-			else if(type == "caractere" && strstr(input,"\""))
-		  		value = splitChar(splitValue,"\"","\"");
-		}
-		if(aux > 1)
-		{
-			input = splitChar(input,",",";");
-			strcat(input, ";");
-		}
-		/*
-		if(checkList(name) == 1)
-		{
-			printf("Nome já existe na tabela de símbolos: %s", nome);
-			return 0;
-		}*/
-		insertSymbol(name, type, value);
-		aux--;
 	}
 	return token;
 }
-
-symbolTable *insertSymbol(char *name, char *type, char *value)
+//Inserts variables into the Symbol Table
+symbolTable *insertSymbol(char *name, char *type, char *value, char *sizeLimit)
 {	
 	if (root == NULL)
 	{
@@ -253,35 +288,27 @@ symbolTable *insertSymbol(char *name, char *type, char *value)
     
     leaf->value = (char *)malloc(strlen(value));
     strncpy(leaf->value, value, strlen(value)+1);
+    
+    leaf->sizeLimit = (char *)malloc(strlen(sizeLimit));
+    strncpy(leaf->sizeLimit, value, strlen(sizeLimit)+1);
     leaf->next == NULL;
     
     size = size + sizeof(leaf->name);
     size = size + sizeof(leaf->value);
     size = size + sizeof(leaf->type);
+    size = size + sizeof(leaf->sizeLimit);
     size = size + sizeof(leaf);
-    printf("\n Nome: %s, Tipo: %s, Valor: %s Insert into table Successful!\n",name, type, value);
+    //printf("\n Nome: %s, Tipo: %s, Valor: %s Insert into table Successful!\n",name, type, value);
     return leaf;
 }
 
-void createList(char *name, char *type, char *value)
+//Creates a list
+void createList(char *name, char *type, char *value, char *sizeLimit)
 {
-	root = insertSymbol(name, type, value);
+	root = insertSymbol(name, type, value, sizeLimit);
 }
 
-int *checkList(char *name)
-{
-    symbolTable *aux;
-    char *nameCheck="teste";
-
-    aux = root;
-    
-    while (aux != NULL) {
-		if(name == nameCheck)
-    		return 1;
-		aux = aux->next;
-    }
-    return 0;
-}
+//Prints all the contents in the list
 void printList()
 {
     	
@@ -296,14 +323,34 @@ void printList()
        i++;
        aux = aux->next; /* próxima célula */
     }
+    free(aux);
 }
 
-
-void checkLine(char *line, int lineNo)
+//Verifies if the variable is already inserted
+int checkInsertVarName(char *name)
 {
+	symbolTable *st = (symbolTable *) malloc(sizeof(symbolTable));
+	//char *auxC = "teste";
+	int i = 1;
+	st = root;
 	
+	while(st != NULL && st->name != NULL)
+	{	
+		if(strcmp(name,st->name) == 0)
+		{
+			printf("\nVariável de nome \"%s\" já foi inserido:\n",st->name);
+			system("pause");
+		}
+		else
+		{
+			st = st->next;
+		}
+	}
+	
+	free(st);
 }
 
+//Check the variable value
 char checkVariable(char *line)
 {
 	
@@ -320,35 +367,76 @@ char checkVariable(char *line)
     return variable;
 }
 
+//Checks the Variable's List
 bool checkVariableList(char *name)
 {
 	bool b;
 }
 
-
+//Searches for the data in the Symbol Table
 bool dataSearch(symbolTable st, char *name)
 {    bool found = false;
 
 	return found;
 }
 
+//Checks if the character is valid
 int checkChar(char *string, char *value)
 {
 	const char *invalidChars = value;
-					   char *mystring = string;
-					   char *character = mystring;
-					   int i = 0;
-					   while (*character)
-					   {
-					       if (strchr(invalidChars, *character))
-					       {
-					         i++;
-					       }					
-					       character++;
-					   }
-		return i;
+	char *mystring = string;
+	char *character = mystring;
+	int i = 0;
+	while (*character)
+	{
+		if (strchr(invalidChars, *character))
+		{
+			i++;
+		}					
+			character++;
+	}
+	return i;
+}
+//Checks if there is a list
+int *checkList(char *name)
+{
+    symbolTable *aux;
+    char *nameCheck="teste";
+
+    aux = root;
+    
+    while (aux != NULL) {
+		if(name == nameCheck)
+    		return 1;
+		aux = aux->next;
+    }
+    return 0;
 }
 
+//Checks if string contains only Numbers
+int checkNumber(const char *string)
+{
+	while (*string) {
+        if (isdigit(*string++) == 0)
+        {
+			return 0;
+		}
+    }
+
+    return 1;
+}
+//Checks if the user is trying to use a reserved word the wrong way
+int testReserved(char *str)
+{
+	
+	if(strstr(str,"programa") || strstr(str,"leia") || strstr(str,"escreva") || strstr(str,"se") || strstr(str,"senao") || strstr(str,"para") || strstr(str,"inteiro") || strstr(str,"caractere") ||strstr(str,"real") || strstr(str,"fim") )
+	{
+		printf("\nA palavra \"%s\" é uma palavra reservada!\n", str);
+		system("pause");
+	}
+}
+
+//Checks if there is a ";" where it supposed to be
 int testSemi(const char *str)
 {
   if(*str && str[strlen(str + 1)] == ';')
